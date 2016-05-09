4d5969130af8bd047c9e3f2183d3de5b1b9bd1c6
amanjsaneja <amanjsaneja@gmail.com>
2016-05-09 03:13:57 -0400

    First Commit Angular Website

---
+++ app/app.js
@@ -0,0 +1,111 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.run(function($rootScope, $window) {
+
+    $rootScope.back = function () {
+
+            $window.history.back();
+
+    };
+});
+myApp.config(function($routeProvider){
+  console.log("something");
+  $routeProvider.caseInsensitiveMatch = true;
+  $routeProvider
+      .when('/', {
+        templateUrl : 'Views/allUsers.html',
+        controller : 'AllUsersController'
+      })
+      .when('/users', {
+        templateUrl : 'Views/allUsers.html',
+        controller : 'AllUsersController'
+      })
+      .when('/users/add', {
+        templateUrl : 'Views/addUser.html',
+        controller : 'AddUserController'
+      })
+      .when('/users/:id', {
+        templateUrl : 'Views/userDetails.html',
+        controller : 'UserController'
+      })
+});
+
+
+myApp.controller('AllUsersController', ['$scope','$http','$log','$location', function($scope,$http,$log, $location) {
+    $scope.listOfUsers = []; // Empty array for users list
+  $scope.moveToAddUser = function(){
+    $location.path('/users/add');
+  }
+  $http({
+    method: 'GET',
+    url: 'http://mocker.egen.io/users',
+  }).then(function successCallback(response) {
+    $log.info("success retrieved list of useres");
+    $scope.listOfUsers = response.data;
+  }, function errorCallback(response) {
+    $log.error("Error loading list of users ");
+  });
+
+}] ); //End of all user controller function
+
+
+myApp.controller('UserController', ['$scope','$http','$log','$routeParams','$location','$timeout', function($scope,$http,$log,$routeParams,$location,$timeout) {
+    $scope.selectedUser = {};
+  $http({
+    method: 'GET',
+    url: 'http://mocker.egen.io/users/'+$routeParams.id,
+  }).then(function successCallback(response) {
+    $log.info("Successful in retrieving user information");
+    $scope.selectedUser = response.data; // Setting the input fields with the retrieved data
+  }, function errorCallback(response) {
+    $log.error("Error fetching information with the User Id provided");
+  });
+
+  $scope.deleted = '';  // Storing the status of delete operation
+
+  $scope.deleteUser = function(){
+    $http({
+      method: 'DELETE',
+      url: 'http://mocker.egen.io/users/'+ $scope.selectedUser.id ,
+    }).then(function successCallback(response) {
+      $log.info("delete success");
+      $scope.deleted = true; // Enabling success message
+      $timeout(function(){
+        $location.path("/users");
+      }, 2000); // After success, take control to the main page after 2 second wait
+    }, function errorCallback(response) {
+      $log.error("fail");
+      $scope.deleted = false; // Enabling the error message
+    });
+  }
+
+}] ); //End for single user controller function
+
+
+
+myApp.controller('AddUserController', ['$scope','$http','$log','$timeout', function($scope,$http,$log, $timeout) {
+  $scope.formData = {}; // Storing form data
+  $scope.added =''; // For storing status of the submit operation
+
+  $scope.submit = function(){
+      console.log("Submitted called");
+    $http({
+      method: 'POST',
+      data : JSON.stringify($scope.formData),
+      url: 'http://mocker.egen.io/users',
+    }).then(function successCallback(response) {
+      $log.info("successfully added new user");
+        console.log("Success called");
+      $scope.added = true; // Enabling success message
+      $scope.formData = {}; // Replacing all fields with empty string
+      // $timeout(function(){
+      //   $scope.added = '';
+      // }, 2000) // After success, slear the success message after 2 seconds
+    }, function errorCallback(response) {
+        console.log("Failed called");
+      $log.error("failed to add new user");
+      $scope.added = false;  // Enabling error message
+    });
+  }
+
+}] ); //End of all user controller function
---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,172 @@
+
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('Http Requests Test', function() {
+  'use strict';
+
+  var $http, $httpBackend, usersCntrl, addCntrl, detailsCntrl, scope;
+    var testData = [
+        {
+            "id": "8ab2f4e9-f289-4e1b-90dc-ab362c3cf917",
+            "firstName": "Enrique",
+            "lastName": "Oberbrunner",
+            "email": "Hadley86@gmail.com",
+            "address": {
+                "street": "259 Mathew Dale",
+                "city": "Melany port"
+            }
+        },
+        {
+            "id": "689fdb71-85a2-433b-bed6-31b34ace08e8",
+            "firstName": "Mylene",
+            "lastName": "Franecki",
+            "email": "Audrey30@gmail.com",
+            "address": {
+                "street": "2757 Gulgowski Knolls",
+                "city": "Terrence mouth"
+            }
+        },
+        {
+            "id": "5de1df8e-593a-4ac6-9291-462739d5d4a6",
+            "firstName": "Ryley",
+            "lastName": "Ward",
+            "email": "Orland_Cummerata59@yahoo.com",
+            "address": {
+                "street": "8394 Russel Shore",
+                "city": "Derek chester"
+            }
+        },
+        {
+            "id": "de33449a-4906-4dcf-8fd3-0c9e6b380eb1",
+            "firstName": "Jennifer",
+            "lastName": "Kautzer",
+            "email": "Elton.Lebsack@yahoo.com",
+            "address": {
+                "street": "22946 Lucienne Flat",
+                "city": "East Itzel ville"
+            }
+        },
+        {
+            "id": "3b5d99ab-ab6d-4941-b61b-b078fc6728db",
+            "firstName": "Marcella",
+            "lastName": "Lang",
+            "email": "Fabiola.Homenick@yahoo.com",
+            "address": {
+                "street": "46064 Morar Crossing",
+                "city": "West Liliana stad"
+            }
+        },
+        {
+            "id": "6e51f1ed-85af-420d-9b3d-ec0965fb7dff",
+            "firstName": "Mervin",
+            "lastName": "Hammes",
+            "email": "Elmore_Cummerata61@yahoo.com",
+            "address": {
+                "street": "6465 Maggio Prairie",
+                "city": "Lake Pearlie"
+            }
+        }
+    ];
+    beforeEach(module("myApp"));
+    beforeEach(inject(function(_$http_, _$httpBackend_ ){
+        $http= _$http_;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    describe('Testing main view', function(){
+
+        beforeEach(inject(function($controller, $rootScope){
+            scope = $rootScope.$new();
+            usersCntrl = $controller("AllUsersController",{
+                $scope :scope
+            });
+        }));
+
+
+        it('Try getting user list', function() {
+            $httpBackend.whenGET("http://mocker.egen.io/users").respond(testData);
+            $httpBackend.flush();
+            expect(scope.listOfUsers.length).toBe(6);
+
+        });
+
+        it('Check if correct fields are updated after retriving users', function() {
+            $httpBackend.whenGET("http://mocker.egen.io/users").respond(testData);
+            $httpBackend.flush();
+            expect(scope.listOfUsers[0].firstName).toBe("Enrique");
+            expect(scope.listOfUsers[5].firstName).toBe("Mervin");
+        });
+
+    });
+
+
+  describe('Check Single User Deails View', function() {
+
+      beforeEach(inject(function($controller, $rootScope){
+          scope = $rootScope.$new();
+          detailsCntrl = $controller("UserController",{
+              $scope :scope
+          });
+      }));
+
+      it('1. Deleted variable is set to "" ', function() {
+          expect(scope.deleted).toBe("");
+      });
+
+      it('2. Try getting user details', function() {
+          $httpBackend.whenGET("http://mocker.egen.io/users/undefined").respond(testData[0]);
+          $httpBackend.flush();
+          expect(scope.selectedUser.firstName).toBe("Enrique");
+      });
+
+      it('3. Delete Operation Success Check', function() {
+          $httpBackend.whenGET("http://mocker.egen.io/users/undefined").respond(testData[0]);
+          $httpBackend.flush();
+          scope.deleteUser();
+          $httpBackend.when('DELETE', 'http://mocker.egen.io/users/'+ scope.selectedUser.id).respond(200, '');
+          $httpBackend.flush();
+          expect(scope.deleted).toBe(true);
+      });
+
+      it('4. Delete Operation Failure Check', function() {
+          $httpBackend.whenGET("http://mocker.egen.io/users/undefined").respond(testData[0]);
+          $httpBackend.flush();
+          scope.deleteUser();
+          $httpBackend.when('DELETE', 'http://mocker.egen.io/users/'+ scope.selectedUser.id).respond(404, '');
+          $httpBackend.flush();
+          expect(scope.deleted).toBe(false);
+      });
+
+  });
+
+    describe('Check Add User View', function() {
+
+        beforeEach(inject(function($controller, $rootScope){
+            scope = $rootScope.$new();
+            addCntrl = $controller("AddUserController",{
+                $scope :scope
+            });
+        }));
+
+        it('1. Check for Form Data Object and Added variable value', function() {
+            expect(Object.keys(scope.formData).length).toBe(0);
+            expect(scope.added).toBe('');
+        });
+
+        it('2. Try Success Add Operation', function() {
+            // $httpBackend.when('POST', 'http://mocker.egen.io/users/').respond(200, '');
+            scope.submit();
+            $httpBackend.when('POST', 'http://mocker.egen.io/users/').respond(200, '');
+            // $httpBackend.whenPOST('http://mocker.egen.io/users',"").respond(200);
+            expect(scope.added).toBe(true);
+        });
+
+        it('3. Try Failed Add Operation', function() {
+            scope.submit();
+            $httpBackend.when('POST', 'http://mocker.egen.io/users/').respond(404, '');
+            expect(scope.added).toBe(false);
+        });
+
+    });
+    
+});
